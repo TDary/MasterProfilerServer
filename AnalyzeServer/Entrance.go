268c21546fefc4a0c65b3e3b0f986d1bc8f32d78
chenDary <10444087+chendary@user.noreply.gitee.com>
2022-10-31 11:39:28 +0800

    配置文件反序列化完善，以及代码计划

--- AnalyzeServer/Entrance.go
+++ AnalyzeServer/Entrance.go
@@ -3,7 +3,6 @@ package AnalyzeServer
 import (
 	"UAutoServer/Logs"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 )
 
@@ -11,21 +10,22 @@ type AllProfilerClient struct {
 	Ip            string
 	WorkerNumbers int
 	WorkType      string
+	State         bool
 }
 
-type ConfigData map[string]interface{}
+type MainTable struct {
+	AppKey   string
+	UUID     string
+	RawFiles []string
+}
+
+type ConfigData struct {
+	Client []AllProfilerClient
+}
 
 func Run() {
 	InitClient()
 	//HttpServer.ListenAndServer("10.11.144.31:8201")
-}
-
-func AnalyzeRequestUrl() {
-
-}
-
-func AddAnalyzeClient() {
-
 }
 
 func InitClient() {
@@ -36,7 +36,23 @@ func InitClient() {
 	if err != nil {
 		Logs.Error(err)
 	}
-	res := config["client"]
+	//测试是否反序列化成功
+	// fmt.Print(config.Client[0].Ip)
+}
 
-	fmt.Print(res)
+//解析url进行结构化
+func AnalyzeRequestUrl() {
+	//从消息队列中取出解析的url进行操作
+	//此处作为消费者,同时调用DataBase创建数据库表
+	for true {
+
+	}
+}
+
+func AddAnalyzeClient() {
+	//当客户端解析器启动时会ping一次服务器，测试是否已将客户端解析器加入了组网,保证机器间正常运行
+	//只有当ping通的情况下才会将开关打开
+	//同时可以加入新的解析客户端
+	//同时在此处进行启动客户端解析
+
 }
--- AnalyzeServer/ParseBegin.go
+++ AnalyzeServer/ParseBegin.go
@@ -1,5 +1,6 @@
 package AnalyzeServer
 
 func SendBeginMessage() {
-
+	//发送开始解析的相关数据信息
+	//前提是已经创建好数据表
 }
