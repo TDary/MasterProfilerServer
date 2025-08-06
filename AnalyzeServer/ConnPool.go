77f8522eaeeee372ce64bbe4317a9ee048ee54f9
chenderui <chenderui1@thewesthill.net>
2025-08-06 17:14:12 +0800

    增加日志以及优化代码

--- AnalyzeServer/ConnPool.go
+++ AnalyzeServer/ConnPool.go
@@ -1,10 +1,11 @@
 package AnalyzeServer
 
 import (
+	"MasterServer/Logs"
 	"net"
 )
 
-//添加进入连接池
+// 添加进入连接池
 func AddConnectior(conn net.Conn, machine string) {
 	var cPool ConnectPool
 	remoteAddr := conn.RemoteAddr().(*net.TCPAddr)
@@ -23,15 +24,16 @@ func AddConnectior(conn net.Conn, machine string) {
 	if !isHas {
 		allconnector = append(allconnector, cPool)
 	}
+	Logs.Loggers().Print("连接池数量", len(allconnector))
 	//将解析器状态调整为空闲，设置之后可以进行解析操作
 	for key, val := range allAnalyzeClient {
-		if val.Ip == currentIp {
+		if val.Ip == currentIp && machine == "anaclient" {
 			allAnalyzeClient[key].State = "idle"
 		}
 	}
 }
 
-//获取Conn连接池对象
+// 获取Conn连接池对象
 func GetConn(ip string, machine string) net.Conn {
 	for _, val := range allconnector {
 		if val.Ip == ip && machine == val.Marchine {
@@ -41,7 +43,7 @@ func GetConn(ip string, machine string) net.Conn {
 	return nil
 }
 
-//断开连接，去除连接池中的连接
+// 断开连接，去除连接池中的连接
 func CloseConnect(ip string, machine string) {
 	for _, val := range allconnector {
 		if val.Ip == ip && machine == val.Marchine {
--- HttpServer/monitor.go
+++ HttpServer/monitor.go
@@ -24,8 +24,9 @@ func HandleConnection(conn net.Conn) {
 		if err != nil && n == 0 {
 			Logs.Loggers().Printf("Error reading from connection: %s", err.Error())
 			//断开连接，清除池子
+			Logs.Loggers().Print("连接断开，清除连接池中的连接：", remoteIp, ConMachine)
 			AnalyzeServer.CloseConnect(remoteIp, ConMachine)
-			return
+			break
 		}
 		if len(buffer) != 0 {
 			res := string(buffer[:n])
